9b10f1936dcfef285e8fc574ef932cec20437c86
DJEMAI <mohamed.djemai@g.enp.edu.dz>
2018-11-03 20:30:42 -0400

    Add files via upload

---
+++ Repartiteur/src/tp2_cloud/ServeurThread.java
@@ -0,0 +1,37 @@
+package tp2_cloud;
+
+import java.rmi.RemoteException;
+import java.util.Map;
+
+public class ServeurThread extends Thread {
+	private String IP;
+	private Map<String,String> dataServer;
+	private int result=0;
+	public ServeurThread(String IP, Map<String,String> dataServer) {
+		IP=IP;
+		dataServer=dataServer;
+	}
+	public void run() {
+		Communication communication = new Communication();
+		ServerInterface stubServeur1 = communication.loadServerStub(IP);
+		try {
+			result = communication.calcul(stubServeur1, dataServer);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public int getResult() {
+		return result;
+	}
+	public Map<String,String> getDataNotsend(){
+		return dataServer;
+	}
+	public String getIP() {
+		return IP;
+	}
+	public Map<String,String> getData(){
+		return dataServer;
+	}
+	
+}
